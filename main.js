90e25eb0ca0357161a29645a8e9d29b1ee34d47c
Scott Zachariae <scott.zachariae@ibykus.de>
2025-01-28 16:22:57 +0100

    Added confetti effect

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-addValues = () => { localStorage.setItem("stackOverflowVisits", Number(localStorage.getItem("stackOverflowVisits")) + 1) }
-getValues = () => { return (localStorage.getItem("stackOverflowVisits")) }
+const script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.4.0/dist/confetti.browser.min.js';
+document.head.appendChild(script);
 
-if (getValues() == null) {
-    localStorage.setItem("stackOverflowVisits", 0)
+function triggerConfetti() {
+  confetti({
+    particleCount: 600,
+    spread: 150,
+  });
+}
+
+addValues = () => {
+  localStorage.setItem("stackOverflowVisits", Number(localStorage.getItem("stackOverflowVisits"))+1)
+}
+
+
+getValues = () => {
+  return(localStorage.getItem("stackOverflowVisits"))
+}
+
+if(getValues() == null) {
+  localStorage.setItem("stackOverflowVisits", 0)
 }
 
 addValues()
+
 if (Number(getValues()) % 10 == 0) {
-    alert(`Yeay, du hast Stackoverflow ${localStorage.getItem("stackOverflowVisits")} mal besucht!`)
+  script.onload = () => {
+    triggerConfetti();
+  };
+  
+  alert(`Yeay, du hast Stackoverflow ${localStorage.getItem("stackOverflowVisits")} mal besucht!`)
 }
