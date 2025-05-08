531437ac19da31a231dc7eb711acaea8b937d7af
Sköht <97896619+NotScottt@users.noreply.github.com>
2025-05-08 08:19:32 +0200

    added Date logger

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.4.0/dist/confetti.browser.min.js';
 document.head.appendChild(script);
+var firstUseDate = new Date()
 
 function triggerConfetti() {
   confetti({
@@ -9,8 +10,18 @@ function triggerConfetti() {
   });
 }
 
+function logFirstUse() {
+	const firstUse = firstUseDate.toLocaleDateString("de",{year:"2-digit",month:"2-digit", day:"2-digit"});
+	
+	localStorage.setItem("firstScriptUse", firstUse)
+}
+
 addValues = () => {
   localStorage.setItem("stackOverflowVisits", Number(localStorage.getItem("stackOverflowVisits"))+1)
+  
+  if (localStorage.getItem("stackOverflowVisits") === 1) {
+  	logFirstUse();
+  }
 }
 
 
@@ -18,16 +29,20 @@ getValues = () => {
   return(localStorage.getItem("stackOverflowVisits"))
 }
 
-if(getValues() == null) {
+if(getValues() === null) {
   localStorage.setItem("stackOverflowVisits", 0)
 }
 
 addValues()
 
-if (Number(getValues()) % 10 == 0) {
+if (Number(getValues()) % 10 === 0) {
   script.onload = () => {
     triggerConfetti();
   };
   
-  alert(`Yeay, du hast Stackoverflow ${localStorage.getItem("stackOverflowVisits")} mal besucht!`)
+  if (localStorage.getItem("firstScriptUse") !== null) {
+  	alert(`Yeay, du hast Stackoverflow seit dem ${localStorage.getItem("firstScriptUse")} genau ${localStorage.getItem("stackOverflowVisits")} mal besucht!`)
+  } else {
+  	alert(`Yeay, du hast Stackoverflow ${localStorage.getItem("stackOverflowVisits")} mal besucht!`)
+  }
 }
